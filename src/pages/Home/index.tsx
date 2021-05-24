a8f6de79cdecbafd6110e69fef03ebf2d4faa583
Sting Silva <sting@Stings-MacBook-Pro.local>
2021-05-24 15:29:23 -0300

    cartItemsAmount ok

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,11 +23,13 @@ interface CartItemsAmount {
 
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount
+
+    return sumAmount;
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
@@ -55,10 +57,7 @@ const Home = (): JSX.Element => {
             <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
             <span>
-              { product.price.toLocaleString('pt-BR', {
-                style: 'currency',
-                currency: 'BRL'
-              }) }
+              { formatPrice(product.price) }
             </span>
             <button
               type="button"
@@ -67,7 +66,7 @@ const Home = (): JSX.Element => {
             >
               <div data-testid="cart-product-quantity">
                 <MdAddShoppingCart size={16} color="#FFF" />
-                {/* {cartItemsAmount[product.id] || 0} */} 2
+                {cartItemsAmount[product.id] || 0} 
               </div>
 
               <span>ADICIONAR AO CARRINHO</span>
